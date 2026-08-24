f9a920682fa405a03637a02db6d98cfc5d49595e
webastur2026 <abel@webastur.es>
2026-08-24 19:41:31 +0200

    Solucionado import de Footer en layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from 'next';
+import './globals.css';
+import Footer from './Footer';
 
 export const metadata: Metadata = {
   title: 'El sol y la mariposa',
